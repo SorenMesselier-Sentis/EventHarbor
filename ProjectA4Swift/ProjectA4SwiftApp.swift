16145c99ca84e6a0d44778466f25775405d06dfb
Soren <soren.messelier@gmail.com>
2023-11-20 11:27:22 +0100

    Initial Commit

---
+++ ProjectA4Swift/ProjectA4SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectA4SwiftApp.swift
+//  ProjectA4Swift
+//
+//  Created by Soren Messelier-Sentis on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectA4SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
